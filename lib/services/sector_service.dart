4f7842c3ae82f917b37c957e34162e4a145346cc
Mészáros Gergely <grigorijat9026@gmail.com>
2024-02-21 08:50:46 +0100

    #133 - harvest the capability of ordinals for sectors, walls and routes

--- lib/services/sector_service.dart
+++ lib/services/sector_service.dart
@@ -11,26 +11,37 @@ class SectorService {
 
   SectorService(this.repository, this.sectorConverter);
 
-  Future<List<String>> getSectorNames() async {
+  Future<List<String>> getSectorNames({bool ordered = false}) async {
     logger.i("Collecting sector names");
-    List<String> sectorNames = [];
-    List<SectorDto> sectors = await getSectorsWithRoutes();
-    for (SectorDto sector in sectors) {
+    final List<String> sectorNames = [];
+    final List<SectorDto> sectors =
+        await getSectorsWithRoutes(ordered: ordered);
+    for (final SectorDto sector in sectors) {
       sectorNames.add(sector.name);
     }
     logger.i("${sectorNames.length} sector name got collected");
     return sectorNames;
   }
 
-  Future<List<SectorDto>> getSectorsWithRoutes() async {
+  Future<List<SectorDto>> getSectorsWithRoutes({bool ordered = false}) async {
     logger.i("Collecting Sectors with their route info.");
-    List<Sector> sectorsFromDb = await repository.fetchAll();
+    final List<Sector> sectorsFromDb = await repository.fetchAll();
     logger.i("${sectorsFromDb.length} sector(s) got collected.");
     if (sectorsFromDb.isNotEmpty) {
       logger.i("Converting ${sectorsFromDb.length} Sector(s) to Sector DTO(s)");
-      List<SectorDto> sectors = [];
-      for (Sector sector in sectorsFromDb) {
+      final List<SectorDto> sectors = [];
+      for (final Sector sector in sectorsFromDb) {
         sectors.add(sectorConverter.convert(sector));
+      }
+      if (ordered) {
+        logger.d("Ordering sectors and their walls and routes");
+        for (final sector in sectors) {
+          for (final wall in sector.walls) {
+            wall.routes.sort((a, b) => a.ordinal.compareTo(b.ordinal));
+          }
+          sector.walls.sort((a, b) => a.ordinal.compareTo(b.ordinal));
+        }
+        sectors.sort((a, b) => a.ordinal.compareTo(b.ordinal));
       }
       return sectors;
     } else {
